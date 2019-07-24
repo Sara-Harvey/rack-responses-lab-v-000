0fd0e45da1b05598cc17a38ee93e2a2df689bcb1
Sara Harvey <saraharveynyc@gmail.com>
2019-07-24 15:26:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,9 +12,9 @@ class Application
     resp.write "#{num_3}\n"
  
     if num_1==num_2 && num_2==num_3
-      resp.write "Good morning!"
+      resp.write "You Win"
     else
-      resp.write "Good afternoon!"
+      resp.write "You Lose"
     end
  
     resp.finish
