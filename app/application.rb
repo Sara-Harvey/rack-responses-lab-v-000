7055d59b21f56ad0dfc1e9109633625c96134b11
Sara Harvey <saraharveynyc@gmail.com>
2019-07-24 15:58:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
   
-    if Time.now.hour < 12
+    time = Time.now
+    
+    if time.hour < 12
       resp.write "Good morning!"
     else
       resp.write "Good afternoon!"
